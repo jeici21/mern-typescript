59447ddece9d3b04c3465d8c32a224b2238658da
Jorge Castro <david92_jc@yahoo.es>
2023-04-29 16:42:04 -0500

    Creada conexión con MongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
+import mongoose from 'mongoose'
+import Deck from "./models/Deck";
+
+const PORT = 5000
 const app = express()
 
 app.get("/", (req, res) => {
     res.send("gg")
 })
+
 app.get("/hello", (req, res) => {
     res.send("hello world")
 })
 
-app.listen(5000)
+mongoose.connect(
+    'mongodb+srv://flashcardsage:5PzLXCj8v7nLVoKb@cluster0.pjfelzr.mongodb.net/?retryWrites=true&w=majority'
+).then(() => {
+    console.log(`listening on port ${PORT}`)
+    app.listen(PORT)//conectándose a la bd
+})
---
+++ server/src/models/Deck.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+//const ObjectId = mongoose.Types.ObjectId;
+
+const DeckSchema = new Schema({
+    title: String
+});//esquema de la tabla
+
+const Deck = mongoose.model('Deck', DeckSchema);//creando la tabla
+
+export default Deck
