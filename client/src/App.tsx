880c2d811b773b1f403d729dcdbc39f37a86d4ae
Jorge Castro <david92_jc@yahoo.es>
2023-04-29 23:52:59 -0500

    Añadidos métodos post en el front y get en el back

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { FormEvent, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState('')
+
+  async function handleCreateDeck(e: FormEvent) {
+    e.preventDefault()
+    await fetch('http://localhost:5000/decks', {
+      method: 'POST', body: JSON.stringify({ title }), headers: { "Content-Type": "application/json" }
+    })//conectando con el server y guardando el ingreso en formato json
+    setTitle("")//vaciando el input
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor='deck-title'>Título del Deck</label>
+        <input id='deck-title' value={title} onChange={e => setTitle(e.target.value)} />
+        <button>Crear Deck</button>
+      </form>
     </>
   )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,26 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import mongoose from 'mongoose'
 import Deck from "./models/Deck";
 import { config } from 'dotenv'
+import cors from 'cors'
 
 config()
 const PORT = 5000
 const app = express()
 
+app.use(cors({ origin: "*" }))//garantizando el acceso a editar la bd desde cualquier origen
 app.use(express.json())//especificando que los datos se manejarán como json
+
+app.get('/decks',async (req, res) => {
+    const decks = await Deck.find()
+    res.json(decks)
+})//mostrando todos los registros en formato json
 
 app.post("/decks", async (req, res) => {
     const newDeck = new Deck({ title: req.body.title })
     const createdDeck = await newDeck.save()
     res.json(createdDeck)
-})//añadiendo un registro a la bd
+})//añadiendo un registro a la bd en formato json
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
     console.log(`escuchando en el puerto ${PORT}`)
