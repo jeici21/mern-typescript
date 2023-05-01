f69672a9b008740c7737048521efd3a4f4f9cc3d
Jorge Castro <david92_jc@yahoo.es>
2023-05-01 15:45:11 -0500

    Correcciones menores

--- server/src/controllers/createCardForDeckController.ts
+++ server/src/controllers/createCardForDeckController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express"
 import Deck from "../models/Deck"
 
+type Text = { text: string }
+
 export async function createCardForDeckController(req: Request, res: Response) {
     const deckId = req.params.deckId
     const deck = await Deck.findById(deckId)
     if (!deck) return res.status(400).send('No existe un deck con este id')
-    const { text } = req.body as { text: string }
+    const { text }: Text = req.body
     deck.cards.push(text)
     await deck.save()
     res.json(deck)
