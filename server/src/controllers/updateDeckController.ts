a2ce97fb2d0f32ade38e55a1ea374c90ecc4f82c
Jorge Castro <david92_jc@yahoo.es>
2023-05-02 00:03:40 -0500

    correcciones menores

--- server/src/controllers/updateDeckController.ts
+++ server/src/controllers/updateDeckController.ts
@@ -3,8 +3,6 @@ import Deck from "../models/Deck";
 
 export async function updateDeckController(req: Request, res: Response) {
     const deckId = req.params.deckId
-    const deck = await Deck.findByIdAndUpdate(
-        deckId, { $set: { title: req.body.title as string } }, { new: true }
-    )
+    const deck = await Deck.findByIdAndUpdate(deckId, { title: req.body.title as string }, { new: true })
     res.json(deck)
 }//req.body as TDeck como segundo parámetro si quiero actualizar todo el registro
