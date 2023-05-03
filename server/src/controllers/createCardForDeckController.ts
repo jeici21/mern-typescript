5826d7a932577c115e769be4beb12ee671334b03
Jorge Castro <david92_jc@yahoo.es>
2023-05-02 22:52:30 -0500

    Simplificando componentes

--- server/src/controllers/createCardForDeckController.ts
+++ server/src/controllers/createCardForDeckController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express"
-import Deck from "../models/Deck"
+import Deck, { Card } from "../models/Deck"
 
 export async function createCardForDeckController(req: Request, res: Response) {
     const deck = await Deck.findById(req.params.deckId)
     if (!deck) return res.status(400).send('No existe un deck con este id')
-    const { text }: { text: string } = req.body
+    const { text }: Card = req.body
     deck.cards.push(text)
     await deck.save()
     res.json(deck)
--- server/src/models/Deck.ts
+++ server/src/models/Deck.ts
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
 export type TDeck = { title: string, cards: string[] }
+export type Card = { text: string }
 
 const Schema = mongoose.Schema;
-//const ObjectId = mongoose.Types.ObjectId;
-
 const DeckSchema = new Schema({ title: String, cards: [String] });//esquema de la tabla
-
 const Deck = mongoose.model('Deck', DeckSchema);//creando la tabla
 
 export default Deck
